1874f6cdaa5d14881bbcbb6cb68beb31c9ff5224
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-27 23:53:24 +0100

    cambio de fondo mas estilos además del cálculo de arboles por segundos

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 @namespace
 class SpriteKind:
     trading = SpriteKind.create()
-"""
-
----------- VARIABLES ----------
-
-"""
 
 def on_on_overlap(player2, house):
     global in_trading
     in_trading = True
     game.show_long_text("Prem A per comerciar", DialogLayout.BOTTOM)
 sprites.on_overlap(SpriteKind.player, SpriteKind.trading, on_on_overlap)
-
 
 def on_down_pressed():
     animation.run_image_animation(steve,
@@ -22,32 +16,6 @@ def on_down_pressed():
         500,
         False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
-
-def abrir_inventario():
-    global opcio2, quantitat2, resultat
-    opcio2 = Math.floor(game.ask_for_number("""
-        BOTIGA
-        1 Gallina
-        2 Patates
-        3 Cabra
-        4 Ous
-        5 Cavall
-        Escull opcio:
-        """))
-    quantitat2 = Math.floor(game.ask_for_number("Quantes unitats vols?"))
-    resultat = calcular_arbres(opcio2, quantitat2)
-    if resultat == -1:
-        game.show_long_text("Opcio incorrecta", DialogLayout.BOTTOM)
-    elif resultat == -2:
-        game.show_long_text("""
-                Quantitat incorrecta
-                (Solo enters positius)
-                """,
-            DialogLayout.BOTTOM)
-    else:
-        producte = product_names[opcio2 - 1]
-        game.show_long_text("" + str(quantitat2) + " " + producte + "\nnecessiten\n" + ("" + str(resultat)) + " arbres",
-            DialogLayout.FULL)
 
 def on_right_pressed():
     animation.run_image_animation(steve,
@@ -67,10 +35,9 @@ def on_left_pressed():
         False)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
-
 def on_a_pressed():
     if in_trading:
-        abrir_inventario()
+        pass
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
 def on_b_pressed():
@@ -87,6 +54,8 @@ def calcular_arbres(opcio: number, quantitat: number):
         return -2
     arbres = product_values[index] * quantitat
     return Math.round(arbres * 100) / 100
+def jugador_quieto():
+    return steve.vx == 0 and steve.vy == 0
 
 def on_up_pressed():
     animation.run_image_animation(steve,
@@ -97,24 +66,26 @@ def on_up_pressed():
         False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
+def esta_sobre_arbol():
+    return steve.overlaps_with(arbol) or steve.overlaps_with(arbol2) or steve.overlaps_with(arbol3)
+troncos = 0
+temps_parat = 0
 arbres = 0
 index = 0
-resultat = 0
-quantitat2 = 0
-opcio2 = 0
 in_trading = False
+arbol3: Sprite = None
+arbol2: Sprite = None
+arbol: Sprite = None
 steve: Sprite = None
 product_values: List[number] = []
-product_names: List[str] = []
 product_names = ["Gallina",
     "Patates (1.5 kg)",
     "Cabra",
     "Dotzena d'ous",
     "Cavall"]
 product_values = [6, 2, 5, 3, 12]
-
 scene.set_background_image(assets.image("""
-    background
+    fons
     """))
 steve = sprites.create(assets.image("""
     pers
@@ -131,8 +102,27 @@ arbol2 = sprites.create(assets.image("""
 arbol3 = sprites.create(assets.image("""
     arbol
     """), SpriteKind.enemy)
-arbol3.set_position(144, 82)
-arbol2.set_position(112, 82)
-arbol.set_position(80, 82)
+arbol3.set_position(116, 36)
+arbol2.set_position(144, 90)
+arbol.set_position(80, 75)
 trade.set_position(25, 82)
 controller.move_sprite(steve, 100, 100)
+icono_llenya = sprites.create(assets.image("""
+    llenya
+    """), SpriteKind.food)
+icono_llenya.set_position(150, 10)
+icono_llenya.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
+info.set_score(0)
+
+def on_on_update():
+    global temps_parat, troncos
+    if esta_sobre_arbol() and jugador_quieto():
+        temps_parat += 1
+        if temps_parat >= 120:
+            # ~3 segons parat al costat de l'arbre
+            troncos += 1
+            info.set_score(troncos)
+            temps_parat = 0
+    else:
+        temps_parat = 0
+game.on_update(on_on_update)
--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 namespace SpriteKind {
     export const trading = SpriteKind.create()
 }
-// ---------- VARIABLES ----------
 sprites.onOverlap(SpriteKind.Player, SpriteKind.trading, function (player2, house) {
     in_trading = true
     game.showLongText("Prem A per comerciar", DialogLayout.Bottom)
@@ -14,31 +13,6 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
-function abrir_inventario () {
-    let producte: string;
-opcio2 = Math.floor(game.askForNumber(`
-        BOTIGA
-        1 Gallina
-        2 Patates
-        3 Cabra
-        4 Ous
-        5 Cavall
-        Escull opcio:
-        `))
-    quantitat2 = Math.floor(game.askForNumber("Quantes unitats vols?"))
-    resultat = calcular_arbres(opcio2, quantitat2)
-    if (resultat == -1) {
-        game.showLongText("Opcio incorrecta", DialogLayout.Bottom)
-    } else if (resultat == -2) {
-        game.showLongText(`
-                Quantitat incorrecta
-                (Solo enters positius)
-                `, DialogLayout.Bottom)
-    } else {
-        producte = product_names[opcio2 - 1]
-        game.showLongText("" + quantitat2 + " " + producte + "\nnecessiten\n" + ("" + resultat) + " arbres", DialogLayout.Full)
-    }
-}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     steve,
@@ -57,7 +31,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (in_trading) {
-        abrir_inventario()
+    	
     }
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -74,6 +48,9 @@ function calcular_arbres (opcio: number, quantitat: number) {
     arbres = product_values[index] * quantitat
     return Math.round(arbres * 100) / 100
 }
+function jugador_quieto () {
+    return steve.vx == 0 && steve.vy == 0
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     steve,
@@ -82,16 +59,20 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+function esta_sobre_arbol () {
+    return steve.overlapsWith(arbol) || steve.overlapsWith(arbol2) || steve.overlapsWith(arbol3)
+}
+let troncos = 0
+let temps_parat = 0
 let arbres = 0
 let index = 0
-let resultat = 0
-let quantitat2 = 0
-let opcio2 = 0
 let in_trading = false
+let arbol3: Sprite = null
+let arbol2: Sprite = null
+let arbol: Sprite = null
 let steve: Sprite = null
 let product_values: number[] = []
-let product_names: string[] = []
-product_names = [
+let product_names = [
 "Gallina",
 "Patates (1.5 kg)",
 "Cabra",
@@ -105,14 +86,31 @@ product_values = [
 3,
 12
 ]
-scene.setBackgroundImage(assets.image`background`)
+scene.setBackgroundImage(assets.image`fons`)
 steve = sprites.create(assets.image`pers`, SpriteKind.Player)
 let trade = sprites.create(assets.image`trade`, SpriteKind.trading)
-let arbol = sprites.create(assets.image`arbol`, SpriteKind.Enemy)
-let arbol2 = sprites.create(assets.image`arbol`, SpriteKind.Enemy)
-let arbol3 = sprites.create(assets.image`arbol`, SpriteKind.Enemy)
-arbol3.setPosition(144, 82)
-arbol2.setPosition(112, 82)
-arbol.setPosition(80, 82)
+arbol = sprites.create(assets.image`arbol`, SpriteKind.Enemy)
+arbol2 = sprites.create(assets.image`arbol`, SpriteKind.Enemy)
+arbol3 = sprites.create(assets.image`arbol`, SpriteKind.Enemy)
+arbol3.setPosition(116, 36)
+arbol2.setPosition(144, 90)
+arbol.setPosition(80, 75)
 trade.setPosition(25, 82)
 controller.moveSprite(steve, 100, 100)
+let icono_llenya = sprites.create(assets.image`llenya`, SpriteKind.Food)
+icono_llenya.setPosition(150, 10)
+icono_llenya.setFlag(SpriteFlag.StayInScreen, true)
+info.setScore(0)
+game.onUpdate(function () {
+    if (esta_sobre_arbol() && jugador_quieto()) {
+        temps_parat += 1
+        if (temps_parat >= 120) {
+            // ~3 segons parat al costat de l'arbre
+            troncos += 1
+            info.setScore(troncos)
+            temps_parat = 0
+        }
+    } else {
+        temps_parat = 0
+    }
+})
