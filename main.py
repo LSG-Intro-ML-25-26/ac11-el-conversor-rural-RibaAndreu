2265f8466ad4c64b97c201177c91e7a0713096d6
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-28 17:12:40 +0100

    menú principal más correcciones del trading en la casa

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 @namespace
 class SpriteKind:
     trading = SpriteKind.create()
+    menu_ui = SpriteKind.create()
 
 def on_on_overlap(player2, house):
     global in_trading
-    in_trading = True
-    steve.say("A: Botiga", 200)
+    if joc_iniciat:
+        in_trading = True
+        steve.say("A: Botiga", 100)
 sprites.on_overlap(SpriteKind.player, SpriteKind.trading, on_on_overlap)
 
 def obrir_botiga():
     global i, opcio, idx, troncos
+    steve.say("")
     llista = "BOTIGA (Preus):\n"
-    while i <= len(product_names) - 1:
-        llista = "" + llista + ("" + str((i + 1))) + ". " + product_names[i] + " (" + ("" + str(product_values[i])) + ")\n"
+    while i < len(product_names):
+        llista = "" + llista + "" + ("" + str((i + 1))) + ". " + product_names[i] + " (" + ("" + str(product_values[i])) + ")\n"
         i += 1
     game.show_long_text(llista, DialogLayout.CENTER)
     opcio = game.ask_for_number("Quin producte vols? (1-5)", 1)
@@ -32,113 +35,135 @@ def obrir_botiga():
                     DialogLayout.BOTTOM)
     else:
         game.show_long_text("Opció no vàlida", DialogLayout.BOTTOM)
+    steve.say("")
 
 def on_down_pressed():
-    animation.run_image_animation(steve,
-        assets.animation("""
-            nena-animation-down
-            """),
-        500,
-        False)
+    if joc_iniciat:
+        animation.run_image_animation(steve,
+            assets.animation("""
+                nena-animation-down
+                """),
+            500,
+            False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 def on_right_pressed():
-    animation.run_image_animation(steve,
-        assets.animation("""
-            nena-animation-right
-            """),
-        500,
-        False)
+    if joc_iniciat:
+        animation.run_image_animation(steve,
+            assets.animation("""
+                nena-animation-right
+                """),
+            500,
+            False)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
 def on_left_pressed():
-    animation.run_image_animation(steve,
-        assets.animation("""
-            nena-animation-left
-            """),
-        500,
-        False)
+    if joc_iniciat:
+        animation.run_image_animation(steve,
+            assets.animation("""
+                nena-animation-left
+                """),
+            500,
+            False)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
 def on_a_pressed():
-    if in_trading:
+    if not (joc_iniciat):
+        iniciar_partida()
+    elif in_trading:
         obrir_botiga()
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
-def jugador_quieto():
-    return steve.vx == 0 and steve.vy == 0
+def mostrar_menu_inicial():
+    global fons_blanc, targeta_menu
+    fons_blanc = image.create(140, 90)
+    fons_blanc.fill(1)
+    fons_blanc.print("LA GRANJA DE SORIA", 15, 15, 15)
+    fons_blanc.print("-----------------", 15, 30, 15)
+    fons_blanc.print("Mou-te amb fletxes", 15, 50, 15)
+    fons_blanc.print("PREM A PER JUGAR", 20, 70, 15)
+    targeta_menu = sprites.create(fons_blanc, SpriteKind.menu_ui)
+    targeta_menu.set_position(80, 60)
+    targeta_menu.z = 200
+def iniciar_partida():
+    global joc_iniciat, steve, trade, arbol, arbol2, arbol3, icono_llenya
+    joc_iniciat = True
+    targeta_menu.destroy()
+    scene.set_background_image(assets.image("""
+        fons
+        """))
+    steve = sprites.create(assets.image("""
+        pers
+        """), SpriteKind.player)
+    steve.set_stay_in_screen(True)
+    steve.z = 100
+    controller.move_sprite(steve, 100, 100)
+    trade = sprites.create(assets.image("""
+        trade
+        """), SpriteKind.trading)
+    arbol = sprites.create(assets.image("""
+        arbol
+        """), SpriteKind.enemy)
+    arbol2 = sprites.create(assets.image("""
+        arbol
+        """), SpriteKind.enemy)
+    arbol3 = sprites.create(assets.image("""
+        arbol
+        """), SpriteKind.enemy)
+    arbol3.set_position(116, 36)
+    arbol2.set_position(140, 90)
+    arbol.set_position(80, 75)
+    trade.set_position(25, 82)
+    icono_llenya = sprites.create(assets.image("""
+        llenya
+        """), SpriteKind.food)
+    icono_llenya.set_position(150, 10)
+    icono_llenya.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
+    info.set_score(0)
 
 def on_up_pressed():
-    animation.run_image_animation(steve,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
+    if joc_iniciat:
+        animation.run_image_animation(steve,
+            assets.animation("""
+                nena-animation-up
+                """),
+            500,
+            False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
-def esta_sobre_arbol():
-    return steve.overlaps_with(arbol) or steve.overlaps_with(arbol2) or steve.overlaps_with(arbol3)
 temps_parat = 0
+icono_llenya: Sprite = None
+arbol3: Sprite = None
+arbol2: Sprite = None
+arbol: Sprite = None
+trade: Sprite = None
+targeta_menu: Sprite = None
 troncos = 0
 idx = 0
 opcio = 0
 i = 0
-in_trading = False
-arbol3: Sprite = None
-arbol2: Sprite = None
-arbol: Sprite = None
 steve: Sprite = None
+in_trading = False
+joc_iniciat = False
 product_values: List[number] = []
 product_names: List[str] = []
+fons_blanc: Image = None
 product_names = ["Gallina", "Patates", "Cabra", "Ous", "Cavall"]
 product_values = [6, 2, 5, 3, 12]
-scene.set_background_image(assets.image("""
-    fons
-    """))
-steve = sprites.create(assets.image("""
-    pers
-    """), SpriteKind.player)
-steve.set_stay_in_screen(True)
-steve.z = 100
-trade = sprites.create(assets.image("""
-    trade
-    """), SpriteKind.trading)
-arbol = sprites.create(assets.image("""
-    arbol
-    """), SpriteKind.enemy)
-arbol2 = sprites.create(assets.image("""
-    arbol
-    """), SpriteKind.enemy)
-arbol3 = sprites.create(assets.image("""
-    arbol
-    """), SpriteKind.enemy)
-arbol3.set_position(116, 36)
-arbol2.set_position(140, 90)
-arbol.set_position(80, 75)
-trade.set_position(25, 82)
-controller.move_sprite(steve, 100, 100)
-icono_llenya = sprites.create(assets.image("""
-    llenya
-    """), SpriteKind.food)
-icono_llenya.set_position(150, 10)
-icono_llenya.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
-info.set_score(0)
+mostrar_menu_inicial()
 
 def on_on_update():
-    global temps_parat, troncos
-    if esta_sobre_arbol() and jugador_quieto():
-        temps_parat += 1
-        if temps_parat >= 120:
-            troncos += 1
-            info.set_score(troncos)
+    global temps_parat, troncos, in_trading
+    if joc_iniciat and steve:
+        if (steve.overlaps_with(arbol) or steve.overlaps_with(arbol2) or steve.overlaps_with(arbol3)) and steve.vx == 0 and steve.vy == 0:
+            temps_parat += 1
+            if temps_parat >= 120:
+                troncos += 1
+                info.set_score(troncos)
+                temps_parat = 0
+        else:
             temps_parat = 0
-    else:
-        temps_parat = 0
+        if not (steve.overlaps_with(trade)):
+            in_trading = False
+            steve.say("")
 game.on_update(on_on_update)
-
-def on_on_update2():
-    global in_trading
-    if not (steve.overlaps_with(trade)):
-        in_trading = False
-game.on_update(on_on_update2)
--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
 namespace SpriteKind {
     export const trading = SpriteKind.create()
+    export const menu_ui = SpriteKind.create()
 }
 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.trading, function on_on_overlap(player2: Sprite, house: Sprite) {
     
-    in_trading = true
-    steve.say("A: Botiga", 200)
+    if (joc_iniciat) {
+        in_trading = true
+        steve.say("A: Botiga", 100)
+    }
+    
 })
 function obrir_botiga() {
     let quantitat: number;
     let preu_total: number;
     
+    steve.say("")
     let llista = "BOTIGA (Preus):\n"
-    while (i <= product_names.length - 1) {
-        llista = "" + llista + ("" + ("" + (i + 1))) + ". " + product_names[i] + " (" + ("" + ("" + product_values[i])) + ")\n"
+    while (i < product_names.length) {
+        llista = "" + llista + "" + ("" + ("" + (i + 1))) + ". " + product_names[i] + " (" + ("" + ("" + product_values[i])) + ")\n"
         i += 1
     }
     game.showLongText(llista, DialogLayout.Center)
@@ -38,106 +43,139 @@ function obrir_botiga() {
         game.showLongText("Opció no vàlida", DialogLayout.Bottom)
     }
     
+    steve.say("")
 }
 
 controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
-    animation.runImageAnimation(steve, assets.animation`
-            nena-animation-down
-            `, 500, false)
+    if (joc_iniciat) {
+        animation.runImageAnimation(steve, assets.animation`
+                nena-animation-down
+                `, 500, false)
+    }
+    
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(steve, assets.animation`
-            nena-animation-right
-            `, 500, false)
+    if (joc_iniciat) {
+        animation.runImageAnimation(steve, assets.animation`
+                nena-animation-right
+                `, 500, false)
+    }
+    
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(steve, assets.animation`
-            nena-animation-left
-            `, 500, false)
+    if (joc_iniciat) {
+        animation.runImageAnimation(steve, assets.animation`
+                nena-animation-left
+                `, 500, false)
+    }
+    
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    if (in_trading) {
+    if (!joc_iniciat) {
+        iniciar_partida()
+    } else if (in_trading) {
         obrir_botiga()
     }
     
 })
-function jugador_quieto() {
-    return steve.vx == 0 && steve.vy == 0
+function mostrar_menu_inicial() {
+    
+    fons_blanc = image.create(140, 90)
+    fons_blanc.fill(1)
+    fons_blanc.print("LA GRANJA DE SORIA", 15, 15, 15)
+    fons_blanc.print("-----------------", 15, 30, 15)
+    fons_blanc.print("Mou-te amb fletxes", 15, 50, 15)
+    fons_blanc.print("PREM A PER JUGAR", 20, 70, 15)
+    targeta_menu = sprites.create(fons_blanc, SpriteKind.menu_ui)
+    targeta_menu.setPosition(80, 60)
+    targeta_menu.z = 200
+}
+
+function iniciar_partida() {
+    
+    joc_iniciat = true
+    targeta_menu.destroy()
+    scene.setBackgroundImage(assets.image`
+        fons
+        `)
+    steve = sprites.create(assets.image`
+        pers
+        `, SpriteKind.Player)
+    steve.setStayInScreen(true)
+    steve.z = 100
+    controller.moveSprite(steve, 100, 100)
+    trade = sprites.create(assets.image`
+        trade
+        `, SpriteKind.trading)
+    arbol = sprites.create(assets.image`
+        arbol
+        `, SpriteKind.Enemy)
+    arbol2 = sprites.create(assets.image`
+        arbol
+        `, SpriteKind.Enemy)
+    arbol3 = sprites.create(assets.image`
+        arbol
+        `, SpriteKind.Enemy)
+    arbol3.setPosition(116, 36)
+    arbol2.setPosition(140, 90)
+    arbol.setPosition(80, 75)
+    trade.setPosition(25, 82)
+    icono_llenya = sprites.create(assets.image`
+        llenya
+        `, SpriteKind.Food)
+    icono_llenya.setPosition(150, 10)
+    icono_llenya.setFlag(SpriteFlag.StayInScreen, true)
+    info.setScore(0)
 }
 
 controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
-    animation.runImageAnimation(steve, assets.animation`
-            nena-animation-up
-            `, 500, false)
+    if (joc_iniciat) {
+        animation.runImageAnimation(steve, assets.animation`
+                nena-animation-up
+                `, 500, false)
+    }
+    
 })
-function esta_sobre_arbol() {
-    return steve.overlapsWith(arbol) || steve.overlapsWith(arbol2) || steve.overlapsWith(arbol3)
-}
-
 let temps_parat = 0
+let icono_llenya : Sprite = null
+let arbol3 : Sprite = null
+let arbol2 : Sprite = null
+let arbol : Sprite = null
+let trade : Sprite = null
+let targeta_menu : Sprite = null
 let troncos = 0
 let idx = 0
 let opcio = 0
 let i = 0
-let in_trading = false
-let arbol3 : Sprite = null
-let arbol2 : Sprite = null
-let arbol : Sprite = null
 let steve : Sprite = null
+let in_trading = false
+let joc_iniciat = false
 let product_values : number[] = []
 let product_names : string[] = []
+let fons_blanc : Image = null
 product_names = ["Gallina", "Patates", "Cabra", "Ous", "Cavall"]
 product_values = [6, 2, 5, 3, 12]
-scene.setBackgroundImage(assets.image`
-    fons
-    `)
-steve = sprites.create(assets.image`
-    pers
-    `, SpriteKind.Player)
-steve.setStayInScreen(true)
-steve.z = 100
-let trade = sprites.create(assets.image`
-    trade
-    `, SpriteKind.trading)
-arbol = sprites.create(assets.image`
-    arbol
-    `, SpriteKind.Enemy)
-arbol2 = sprites.create(assets.image`
-    arbol
-    `, SpriteKind.Enemy)
-arbol3 = sprites.create(assets.image`
-    arbol
-    `, SpriteKind.Enemy)
-arbol3.setPosition(116, 36)
-arbol2.setPosition(140, 90)
-arbol.setPosition(80, 75)
-trade.setPosition(25, 82)
-controller.moveSprite(steve, 100, 100)
-let icono_llenya = sprites.create(assets.image`
-    llenya
-    `, SpriteKind.Food)
-icono_llenya.setPosition(150, 10)
-icono_llenya.setFlag(SpriteFlag.StayInScreen, true)
-info.setScore(0)
+mostrar_menu_inicial()
 game.onUpdate(function on_on_update() {
     
-    if (esta_sobre_arbol() && jugador_quieto()) {
-        temps_parat += 1
-        if (temps_parat >= 120) {
-            troncos += 1
-            info.setScore(troncos)
+    if (joc_iniciat && steve) {
+        if ((steve.overlapsWith(arbol) || steve.overlapsWith(arbol2) || steve.overlapsWith(arbol3)) && steve.vx == 0 && steve.vy == 0) {
+            temps_parat += 1
+            if (temps_parat >= 120) {
+                troncos += 1
+                info.setScore(troncos)
+                temps_parat = 0
+            }
+            
+        } else {
             temps_parat = 0
         }
         
-    } else {
-        temps_parat = 0
-    }
-    
-})
-game.onUpdate(function on_on_update2() {
-    
-    if (!steve.overlapsWith(trade)) {
-        in_trading = false
+        if (!steve.overlapsWith(trade)) {
+            in_trading = false
+            steve.say("")
+        }
+        
     }
     
 })
